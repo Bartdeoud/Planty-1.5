25556e914f22b581dea75a5218a7e3fcf75a0b92
bart <bartdeoud@gmail.com>
2021-12-12 21:28:46 +0100

    Commit Login / Signin

--- app/src/main/java/com/example/myapplication/LogIn.java
+++ app/src/main/java/com/example/myapplication/LogIn.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -44,15 +45,22 @@ public class LogIn extends AppCompatActivity {
 
     public boolean CheckLoginData(){
         boolean autenticated = false;
-
-        //in - inside / out -outside
         EditText inMailET = (EditText) findViewById(R.id.inputEmailAddress);
+        EditText inPasswordET = (EditText) findViewById(R.id.inputPassword);
+        if(TextUtils.isEmpty(inMailET.getText())){
+            inMailET.setError("Empty");
+        }
+        if(TextUtils.isEmpty(inPasswordET.getText())){
+            inPasswordET.setError("Empty");
+        }
+        //in - inside / out -outside
+
         String Inmail = inMailET.getText().toString();
 
         //checks with database
         String outPassword = "1";
 
-        EditText inPasswordET = (EditText) findViewById(R.id.inputPassword);
+
         String InPassword = inPasswordET.getText().toString();
 
         if ((InPassword.toString()).equals(outPassword)){
--- app/src/main/java/com/example/myapplication/SignUp.java
+++ app/src/main/java/com/example/myapplication/SignUp.java
@@ -4,10 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class SignUp extends AppCompatActivity {
+
+    public static EditText firstName;
+    public static EditText lastName;
+    public static EditText address;
+    public static EditText company;
+    public static EditText telephone;
+    public static EditText email;
+    public static EditText password;
+    public static EditText passwordConfirm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +29,66 @@ public class SignUp extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                firstName = (EditText) findViewById(R.id.input_FirstName);
+                lastName = (EditText) findViewById(R.id.input_LastName);
+                address = (EditText) findViewById(R.id.input_Address);
+                company = (EditText) findViewById(R.id.input_Company);
+                telephone = (EditText) findViewById(R.id.input_Telephone);
+                email = (EditText) findViewById(R.id.input_Email);
+                password = (EditText) findViewById(R.id.input_Password);
+                passwordConfirm = (EditText) findViewById(R.id.input_ConfPassword);
+
+
+                if(checkList()){
+                    Intent loginpage = new Intent(SignUp.this, LogIn.class);
+                    startActivity(loginpage);
+                }
+
             }
         });
+    }
+    public static boolean checkList(){
+        boolean authenticated = false;
+        boolean filled = true;
+        boolean same = false;
+        //check if empty
+            if(TextUtils.isEmpty(firstName.getText())){
+                firstName.setError("Empty");
+                filled = false;
+            }
+            if(TextUtils.isEmpty(lastName.getText())){
+                lastName.setError("Empty");
+                filled = false;
+            }
+            if(TextUtils.isEmpty(address.getText())){
+                address.setError("Empty");
+                filled = false;
+            }
+            if(TextUtils.isEmpty(telephone.getText())){
+                telephone.setError("Empty");
+                filled = false;
+            }
+            if(TextUtils.isEmpty(email.getText())){
+                email.setError("Empty");
+                filled = false;
+            }
+            if(TextUtils.isEmpty(password.getText())){
+                password.setError("Empty");
+                filled = false;
+            }
+            if(TextUtils.isEmpty(passwordConfirm.getText())){
+                passwordConfirm.setError("Empty");
+                filled = false;
+            }
+
+        //check if password are the same
+        if(password.equals(passwordConfirm) && !password.toString().isEmpty()){
+            same = true;
+        }
+        //checks if evryting is a go
+        if(same && filled){
+            authenticated = true;
+        }
+        return authenticated;
     }
 }

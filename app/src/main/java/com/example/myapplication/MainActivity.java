e7284fac741650feabfb3e0a4662515e77a2a6e5
bart <bartdeoud@gmail.com>
2021-12-13 13:03:04 +0100

    Commit database

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -17,10 +17,30 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import android.annotation.SuppressLint;
+import android.os.StrictMode;
+import android.util.Log;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+//extra
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    //extra
+    Connection connect;
+    String ConnectionResult = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +61,30 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
-
     }
 
+    public void GetTechtFromSQL(View view) {
+        TextView tx1 = (TextView) findViewById(R.id.textView2);
+        TextView tx2 = (TextView) findViewById(R.id.textView9);
+
+        try {
+            ConnectionHelper connectionHelper = new ConnectionHelper();
+            connect = connectionHelper.Connectionclass();
+            if (connect != null) {
+                String query = "Select * from bedrijf";
+                Statement st = connect.createStatement();
+                ResultSet rs = st.executeQuery(query);
+
+                while (rs.next()) {
+                    tx1.setText(rs.getString(1));
+                    tx2.setText(rs.getString(2));
+                }
+            } else {
+                ConnectionResult = "Check Connection";
+            }
+        } catch (Exception ex) {
+            Log.e("Error ", ex.getMessage());
+        }
+
+    }
 }

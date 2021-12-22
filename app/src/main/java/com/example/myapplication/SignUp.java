c7f8aee766fe9118fa152791a5c2035e08ed266f
bart <bartdeoud@gmail.com>
2021-12-22 22:04:10 +0100

    Final Commit Signin

--- app/src/main/java/com/example/myapplication/SignUp.java
+++ app/src/main/java/com/example/myapplication/SignUp.java
@@ -20,14 +20,15 @@ import java.sql.PreparedStatement;
 
 public class SignUp extends AppCompatActivity {
 
-    public static EditText firstName;
-    public static EditText lastName;
-    public static EditText address;
-    public static EditText company;
-    public static EditText telephone;
-    public static EditText email;
-    public static EditText password;
-    public static EditText passwordConfirm;
+    public EditText firstName;
+    public EditText lastName;
+    public EditText address;
+    public EditText company;
+    public EditText telephone;
+    public EditText email;
+    public EditText password;
+    public EditText passwordConfirm;
+    public String Bedrijfscode = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,27 +50,11 @@ public class SignUp extends AppCompatActivity {
                 password = (EditText) findViewById(R.id.input_Password);
                 passwordConfirm = (EditText) findViewById(R.id.input_ConfPassword);
 
-
                 if(SignInValidator()){
                     int Gebruikersnummer = 0;
-
-                    try {
-                        ConnectionHelper connectionHelper = new ConnectionHelper();
-                        connect = connectionHelper.Connectionclass();
-                        if (connect != null) {
-                            //query statement
-                            String query = "SELECT Gebruikercode FROM Gebruiker ORDER BY Gebruikercode";
-                            Statement st = connect.createStatement();
-                            ResultSet rs = st.executeQuery(query);
-                            while (rs.next()){
-                                Gebruikersnummer = Integer.parseInt(rs.getString(1).substring(0, rs.getString(1).length() - 2)) + 1;
-                            }
-                        } else {
-                            ConnectionResult = "Check Connection";
-                        }
-                    } catch (Exception ex) {
-                        Log.e("Error ", ex.getMessage());
-                    }
+                    String query1 = "SELECT Gebruikercode FROM Gebruiker ORDER BY Gebruikercode";
+                    String Gebruikerscode = commitQuery(query1);
+                    Gebruikersnummer = Integer.parseInt(Gebruikerscode.substring(0, Gebruikerscode.length() - 2)) + 1;
 
                     try {
                         ConnectionHelper connectionHelper = new ConnectionHelper();
@@ -77,9 +62,9 @@ public class SignUp extends AppCompatActivity {
                         if (connect != null) {
                             //query statement
                             Connection con = connectionHelper.Connectionclass();
-                            String query = "INSERT INTO Gebruiker (Gebruikercode, Voornaam, Achternaam, Telefoonnummer, Email, Wachtwoord, Bedrijf, Adres) VALUES ('" + Gebruikersnummer + "', '" + firstName.getText() + "', '" + lastName.getText() + "', '" + telephone.getText() + "', '" + email.getText() + "', '" + password.getText() + "', '" + "1" + "', '" + address.getText() + "')";
-                            System.out.println(query);
-                            PreparedStatement prepsInsertProduct = con.prepareStatement(query);
+                            String query2 = "INSERT INTO Gebruiker (Gebruikercode, Voornaam, Achternaam, Telefoonnummer, Email, Wachtwoord, Bedrijf, Adres) VALUES ('" + Gebruikersnummer + "', '" + firstName.getText() + "', '" + lastName.getText() + "', '" + telephone.getText() + "', '" + email.getText() + "', '" + password.getText() + "', '" + Bedrijfscode + "', '" + address.getText() + "')";
+                            System.out.println(query2);
+                            PreparedStatement prepsInsertProduct = con.prepareStatement(query2);
                             prepsInsertProduct.execute();
                         } else {
                             ConnectionResult = "Check Connection";
@@ -101,97 +86,59 @@ public class SignUp extends AppCompatActivity {
         if (EmailAlredyExist()){
             return false;
         }
-        //if(!bedrijfExist()){
-        //    return false;
-        //}
+        if(!bedrijfExist()){
+            return false;
+        }
         return true;
     }
 
-    public String Bedrijfscode = "";
-
     public boolean bedrijfExist(){
-        Connection connect;
-        String ConnectionResult = "";
-        EditText inBedrijfET = (EditText) findViewById(R.id.input_Company);
-        String inBedrijf = inBedrijfET.getText().toString();
-        String CountEmails = "";
-        //counts the amout of same emails in database
-        try {
-            ConnectionHelper connectionHelper = new ConnectionHelper();
-            connect = connectionHelper.Connectionclass();
-            if (connect != null) {
-                //query statement
-                String query = "SELECT COUNT(Naam) FROM Gebruiker WHERE Naam = '" + inBedrijf + "';";
-                Statement st = connect.createStatement();
-                ResultSet rs = st.executeQuery(query);
-                while (rs.next()) {
-                    //puts query output in string
-                    CountEmails = rs.getString(1).toString();
-                }
-            } else {
-                ConnectionResult = "Check Connection";
-            }
-        } catch (Exception ex) {
-            Log.e("Error ", ex.getMessage());
+        String query1 = "SELECT COUNT(Naam) FROM Bedrijf WHERE Naam = '" + company.getText().toString() + "';";
+        String companyCount = commitQuery(query1);
+        if(Integer.parseInt(companyCount) > 0){
+            String query2 = "SELECT Bedrijfcode FROM Bedrijf WHERE Naam = '" + company.getText().toString() + "';";
+            Bedrijfscode = commitQuery(query2);
+            return true;
         }
-        if(Integer.parseInt(CountEmails) == 0){
-            inBedrijfET.setError("Company does not exist");
-            return false;
-        } else {
-            String companyNumber = "";
-            try {
-                ConnectionHelper connectionHelper = new ConnectionHelper();
-                connect = connectionHelper.Connectionclass();
-                if (connect != null) {
-                    //query statement
-                    String query = "SELECT Bedrijfcode FROM Bedrijf WHERE Naam = '" + inBedrijf + "';";
-                    Statement st = connect.createStatement();
-                    ResultSet rs = st.executeQuery(query);
-                    while (rs.next()) {
-                        //puts query output in string
-                        Bedrijfscode = rs.getString(1).toString();
-                        return true;
-                    }
-                } else {
-                    ConnectionResult = "Check Connection";
-                }
-            } catch (Exception ex) {
-                Log.e("Error ", ex.getMessage());
-            }
-        }
+        company.setError("Could not find company");
         return false;
     }
 
     public boolean EmailAlredyExist(){
-        Connection connect;
-        String ConnectionResult = "";
         EditText inMailET = (EditText) findViewById(R.id.input_Email);
         String Inmail = inMailET.getText().toString();
-        String CountEmails = "";
-        //counts the amout of same emails in database
-        try {
-            ConnectionHelper connectionHelper = new ConnectionHelper();
-            connect = connectionHelper.Connectionclass();
-            if (connect != null) {
-                //query statement
-                String query = "SELECT COUNT(Email) FROM Gebruiker WHERE Email = '" + Inmail + "';";
-                Statement st = connect.createStatement();
-                ResultSet rs = st.executeQuery(query);
-                while (rs.next()) {
-                    //puts query output in string
-                    CountEmails = rs.getString(1).toString();
-                }
-            } else {
-                ConnectionResult = "Check Connection";
-            }
-        } catch (Exception ex) {
-            Log.e("Error ", ex.getMessage());
-        }
+        String query = "SELECT COUNT(Email) FROM Gebruiker WHERE Email = '" + Inmail + "';";
+        String CountEmails = commitQuery(query);
         if(Integer.parseInt(CountEmails) > 0){
             inMailET.setError("Email alredy exist");
             return true;
         }
         return false;
+    }
+
+
+    public String commitQuery(String query){
+        Connection connect;
+        String ConnectionResult = "";
+        String result = "";
+        try {
+            ConnectionHelper connectionHelper = new ConnectionHelper();
+            connect = connectionHelper.Connectionclass();
+            if (connect != null) {
+                //query statement
+                Statement st = connect.createStatement();
+                ResultSet rs = st.executeQuery(query);
+                while (rs.next()) {
+                    //puts query output in string
+                    result = rs.getString(1).toString();
+                }
+            } else {
+                ConnectionResult = "Check Connection";
+            }
+        } catch (Exception ex) {
+            Log.e("Error 001", ex.getMessage());
+        }
+        return result;
     }
 
     public boolean FilledIn(){
